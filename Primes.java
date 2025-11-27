121e985ff9103798cbbbf59d5836e46c29377ff2
Hadar Sarig <hadar.sarig@post.runi.ac.il>
2025-11-27 14:15:16 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,36 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to " + n + ":");
+        
+        boolean[] Prime = new boolean[n + 1];
+        for (int i = 2; i < Prime.length; i++) {
+            Prime[i] = true;
+        }
+        
+        int p = 2;
+        while (p <= Math.sqrt(n)) {
+            if (Prime[p] == true) {
+                int i = p + p;
+                while (i <= n) {
+                    Prime[i] = false; 
+                    i = i + p;        
+                }
+            }
+            
+            p++; 
+        }
+        int count = 0; 
+        
+        for (int i = 2; i <= n; i++) {
+            if (Prime[i] == true) {
+                System.out.println(i); 
+                count++; 
+            }
+        }
+        double percentage = ((double) count / n) * 100;
+        
+        System.out.println("There are " + count + " primes between 2 and " + n + 
+                           " (" + (int)percentage + "% are primes)");
     }
 }
